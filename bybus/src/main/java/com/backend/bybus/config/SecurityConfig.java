148b2e4afbe56dae0e47a950e07e4c57cd09a8d0
Artur Raimundi <79712639+arturraimundi@users.noreply.github.com>
2025-08-12 16:58:40 -0300

    Update SecurityConfig.java

--- bybus/src/main/java/com/backend/bybus/config/SecurityConfig.java
+++ bybus/src/main/java/com/backend/bybus/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+//liberado para testes
 @Configuration
 public class SecurityConfig {
     @Bean
